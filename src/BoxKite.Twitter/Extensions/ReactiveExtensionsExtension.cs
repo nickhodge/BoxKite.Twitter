b67315a2795fa928960c085910bb00573bf5c1ba
Nick Hodge <hodgenick@gmail.com>
2013-05-24 14:12:14 +1000

    smalls

--- src/BoxKite.Twitter/Extensions/ReactiveExtensionsExtension.cs
+++ src/BoxKite.Twitter/Extensions/ReactiveExtensionsExtension.cs
@@ -8,9 +8,9 @@ namespace BoxKite.Twitter.Extensions
     public static class ReactiveExtensionsExtension
     {
         // Brendan doesnt like this, and thinks I should "kill it with fire"
-        // but I am going to leave it in as it balancs the "Subscribe" force with a neat "Publish"
+        // but I am going to leave it in as it balances the "Subscribe" force with a neat "Publish"
         // and also, I grew up in an era of Mac System 7: https://en.wikipedia.org/wiki/Publish_and_Subscribe_(Mac_OS)
-        // which was awesome
+        // which was awesome. And *way* before Brendan's time.
         // and, also because, once implementing this I was feeling lots of emotions. Don Draper Style.
 
         public static void Publish<T>(this Subject<T> tsSubject, T tValue)
