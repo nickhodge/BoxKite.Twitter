ebd128f5ef08a64f0bcbf727a12eded71b364f08
Nick Hodge <hodgenick@gmail.com>
2013-08-01 19:29:46 +1000

    partial

---
+++ src/BoxKite.Twitter/Configuration/Configuration.cs
@@ -0,0 +1,15 @@
+﻿// (c) 2012-2013 Nick Hodge mailto:hodgenick@gmail.com & Brendan Forster
+// License: MS-PL
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxKite.Twitter
+{
+    public class Configuration
+    {
+    }
+}
