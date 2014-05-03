ee6577501685b3387883d039aa7e4cbd5677d727
Nick Hodge <hodgenick@gmail.com>
2014-05-03 10:58:51 +1000

    Universal App
    Make Universal App, 1.3.0 nuget version, Reactive.EventAggregator local
    for Universal

--- src/BoxKite.Twitter.Universal/Properties/AssemblyInfo.cs
+++ src/BoxKite.Twitter.Universal/Properties/AssemblyInfo.cs
@@ -1,7 +1,7 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -14,14 +14,7 @@ using System.Resources;
 [assembly: AssemblyCopyright("Copyright © 2012-2014 Nick Hodge and Brendan Forster")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("225e0c2c-4ac4-4c9c-9bc5-40ff5e4f0689")]
+[assembly: NeutralResourcesLanguage("en")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -30,8 +23,8 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3.0.0")]
 [assembly: AssemblyFileVersion("1.3.0.0")]
-[assembly: NeutralResourcesLanguageAttribute("en-US")]
