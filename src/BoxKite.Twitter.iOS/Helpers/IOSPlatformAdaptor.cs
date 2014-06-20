687e36f2bf8d7cf78652a4a7fc2a65e621506915
Nick Hodge <hodgenick@gmail.com>
2014-06-21 09:00:09 +1000

    iOS 2.0.4 Build

---
+++ src/BoxKite.Twitter.iOS/Helpers/IOSPlatformAdaptor.cs
@@ -0,0 +1,35 @@
+﻿// (c) 2012-2014 Nick Hodge mailto:hodgenick@gmail.com & Brendan Forster
+// License: MS-PL
+
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace BoxKite.Twitter
+{
+	public class IOSPlatformAdaptor : IPlatformAdaptor
+	{
+		public void DisplayAuthInBrowser(string u)
+		{
+			Process.Start(u);
+		}
+
+		public Task<string> AuthWithBroker(string authuri, string callbackuri)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		private HMACSHA1 _hmacsha1;
+
+		public void AssignKey(byte[] key)
+		{
+			_hmacsha1 = new HMACSHA1(key);
+		}
+
+		public byte[] ComputeHash(byte[] buffer)
+		{
+			return _hmacsha1.ComputeHash(buffer);
+		}
+
+	}
+}
--- src/BoxKite.Twitter/Connection/TwitterConnection.cs
+++ src/BoxKite.Twitter/Connection/TwitterConnection.cs
@@ -107,7 +107,34 @@ namespace BoxKite.Twitter
             UserSession = BuildUserSession(twitterConsumerKey, twitterConsumerSecret);
          } 
 #endif
+#if(IOS)
+		public TwitterConnection(string twitterConsumerKey, string twitterConsumerSecret)
+		{
+			PlatformAdaptor = new IOSPlatformAdaptor();
+			ApplicationSession = BuildApplicationSession(twitterConsumerKey, twitterConsumerSecret);
+			UserSession = BuildUserSession(twitterConsumerKey, twitterConsumerSecret);
+		}
 
+		public TwitterConnection(string twitterConsumerKey, string twitterConsumerSecret, IPlatformAdaptor platformAdaptor = null)
+		{
+		PlatformAdaptor = platformAdaptor;
+		ApplicationSession = BuildApplicationSession(twitterConsumerKey, twitterConsumerSecret);
+		UserSession = BuildUserSession(twitterConsumerKey, twitterConsumerSecret);
+		}
+
+		public TwitterConnection(TwitterCredentials twitterCredentials)
+		{
+			PlatformAdaptor = new IOSPlatformAdaptor();
+			TwitterCredentials = twitterCredentials;
+			UserSession = BuildUserSession();
+		}
+
+		public TwitterConnection(string twitterConsumerKey, string twitterConsumerSecret, string xauthusername, string xauthpassword)
+		{
+			PlatformAdaptor = new IOSPlatformAdaptor();
+			UserSession = BuildUserSession(twitterConsumerKey, twitterConsumerSecret);
+		} 
+#endif
         private IApplicationSession BuildApplicationSession(string twitterConsumerKey, string twitterConsumerSecret)
         {
             return ApplicationSession ?? new ApplicationSession(twitterConsumerKey, twitterConsumerSecret);
